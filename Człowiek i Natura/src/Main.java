8e0f907eaeaa6b1a00fb18a9744dab70211149e9
Nyatan <domino.ndg@o2.pl>
2019-05-07 14:56:13 +0200

    Drobna poprawka spawnu i umierania TODO komentarze xD

--- Człowiek i Natura/src/Main.java
+++ Człowiek i Natura/src/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         World world = new World();
+        System.out.println(World.getPoplation());
+        World.getOrganisms().get(1).get(0).die();
+        World.cleanCorpses();
+        System.out.println(World.getPoplation());
     }
 }
